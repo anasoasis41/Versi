3296cea19175579d1c7c672c3701b8b255300df1
Anas RIAHI <anas.riahi@smile-maroc.com>
2019-11-12 18:29:09 +0100

    Add RefrechControll

--- versi-app/controller/TrendingFeedVC.swift
+++ versi-app/controller/TrendingFeedVC.swift
@@ -14,10 +14,17 @@ class TrendingFeedVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    let refreshControll = UIRefreshControl()
+    
     var dataSource = PublishSubject<[Repo]>()
     var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
+        tableView.refreshControl = refreshControll
+        refreshControll.tintColor = #colorLiteral(red: 0.2666666667, green: 0.4509803922, blue: 0.9137254902, alpha: 1)
+        refreshControll.attributedTitle = NSAttributedString(string: "Fetching Hot Github Repos 🔥", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.2666666667, green: 0.4509803922, blue: 0.9137254902, alpha: 1), NSAttributedString.Key.font: UIFont(name: "AvenirNext-DemiBold", size: 16.0)!])
+        refreshControll.addTarget(self, action: #selector(fetchData), for: .valueChanged)
+        
         super.viewDidLoad()
         fetchData()
         dataSource.bind(to: tableView.rx.items(cellIdentifier: "trendingRepoCell")) { (row, repo: Repo, cell: TrendingRepoCell) in
@@ -25,9 +32,10 @@ class TrendingFeedVC: UIViewController {
         }.disposed(by: disposeBag)
     }
 
-    func fetchData() {
+    @objc func fetchData() {
         DownloadService.instance.downloadTrendingRepos { (trendingRepoArray) in
             self.dataSource.onNext(trendingRepoArray)
+            self.refreshControll.endRefreshing()
         }
     }
 
